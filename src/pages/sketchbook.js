fdbd94c4089fedada57b503b2800f61726de26a7
Naveesh Khattar <naveeshkhattar@gmail.com>
2020-10-29 19:16:30 +0530

    add sketchbook page grid

--- src/pages/sketchbook.js
+++ src/pages/sketchbook.js
@@ -1,16 +1,54 @@
 import React from "react"
 import Tabs from "../constants/Tabs.js"
 import '../styles/styles.scss'
+import Masonry from 'react-masonry-css'
+import asphaltWebCover from "../images/asphalt web.png"
+import building from "../images/building.jpg"
+import clouds from "../images/clouds.jpg"
+import dome from "../images/dome.jpg"
+import steps from "../images/steps.jpg"
+import window from "../images/window.jpg"
+import "../styles/sketchbook.scss"
+import Footer from "../components/Footer"
 
 //Import Components
 import Header from "../components/Header"
 
 
-const SketchbookPage = () => ( 
+function SketchbookPage () { 
+    return (
     <div>
         <Header selectedItem={Tabs.SKETCHBOOK}/>
+        <Masonry
+            breakpointCols={2}
+            className="my-masonry-grid"
+            columnClassName="my-masonry-grid_column">
+            <div>
+                <img src={steps} />
+            </div>
+            <div>
+                <img src={building} />
+            </div>
+            <div>
+                <img src={window} />
+            </div>
+            <div>
+                <img src={clouds} />
+            </div>
+            <div>
+                <img src={dome} />
+            </div>
+            <div>
+                <img src={asphaltWebCover} />
+            </div>
+        </Masonry>
+
+        {/* Footer */}
+        <Footer />
+
     </div>
   
-)
+    )
+}
 
 export default SketchbookPage
