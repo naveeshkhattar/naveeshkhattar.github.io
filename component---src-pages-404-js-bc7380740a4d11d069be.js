42eebb6b9b3c5724a224c51a5aa78bad2208817b
Naveesh Khattar <naveeshkhattar@gmail.com>
2020-12-18 19:16:10 +0530

    Delete unnecessary generated files

--- component---src-pages-404-js-bc7380740a4d11d069be.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(n,e,t){"use strict";t.r(e);var u=t("q1tI"),w=t.n(u);e.default=function(){return w.a.createElement("p",null," 404 ")}}}]);
-//# sourceMappingURL=component---src-pages-404-js-bc7380740a4d11d069be.js.map
